48838242437ed208baad9426b49372aa960eaa53
Miguel Jimenez <miguejm@outlook.es>
2021-01-27 00:15:10 -0600

    Variables y comentatios. Se crea .gitignore para evadir ficheros

---
+++ js/02-variables.js
@@ -0,0 +1,9 @@
+//  Variables = Contenedor de información
+
+var pais = "México";
+var continente = "America";
+var antiguedad = 2021;
+var pais_y_continente = pais + ' ' + continente;
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
