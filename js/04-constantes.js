4065e9f588a408e4e39e9f2d2053ca49f8317f30
Miguel Jimenez <miguejm@outlook.es>
2021-01-27 14:38:33 -0600

    constantes

---
+++ js/04-constantes.js
@@ -0,0 +1,12 @@
+'use strict'
+/*
+    Constantes
+    Su valor no puede ser modificado
+*/
+
+var web = "https://victorroblesweb.es";
+const ip = "192.168.0.12"
+
+web = "https://www.google.com";
+
+console.log(web, ip);
