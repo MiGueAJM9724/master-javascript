4af95be89646a1eab75f969050fa49dbb4d2ad49
Miguel Jimenez <miguejm@outlook.es>
2021-01-27 15:05:53 -0600

    operadores y tipos de datos

---
+++ js/05-tipos-de-datos.js
@@ -0,0 +1,24 @@
+'use strict'
+
+//  Operadores
+var numero1 =  7;
+var numero2 = 12;
+var operacion = numero1 + numero2;
+operacion = numero1 - numero2;
+operacion = numero1 * numero2;
+operacion = numero1 / numero2;
+operacion = numero1 % numero2;
+
+alert("Es resultado de la operación es:" + operacion);
+
+//  Tipos de datos
+var numero_entero = 24;
+var cadena_texto = "Hola mundo";
+cadena_texto = 'Hola';
+var verdadero_o_false = true;
+
+var numero_falso = "33.3";
+
+console.log(parseFloat(numero_falso) + 4);
+console.log(Number(numero_falso) + 4);
+console.log(String(numero_entero) + 4);
